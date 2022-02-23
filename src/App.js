b357a095b59adbd8e5057714f5488f69615203bd
Henry Holtgeerts <henryholtgeerts@gmail.com>
2022-02-23 05:37:59 -0600

    fix: improve dimensional acuracy

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ const App = memo(() => {
   return (
     <div className="App">
       <a-scene>
-        <a-entity gltf-model="url(./room2.glb)" rotation="0 180 0" position="0.3 0 -1"></a-entity>
-        <a-entity light="type: spot; penumbra: 0.1; intensity: 0.2;" position="0.08 2.2 -0.05" rotation="-90 0 0"></a-entity>
-        <a-entity light="type: spot; penumbra: 0.1; intensity: 0.2;" position="0.08 2.35 -1.68" rotation="-90 0 0"></a-entity>
-        <a-sphere radius="0.05" position="0.08 2.35 -1.68" color="white" material="color: #fff; shader: flat;"></a-sphere>
-        <a-sphere radius="0.05" position="0.08 2.2 -0.05" color="white" material="color: #fff; shader: flat;"></a-sphere>
+        <a-entity gltf-model="url(./closet.glb)" rotation="0 180 0"></a-entity>
+        <a-entity position="-0.22 2.35 0.21">
+          <a-entity light="type: spot; penumbra: 0.1; intensity: 0.2;" rotation="-90 0 0"></a-entity>
+          <a-sphere radius="0.05" color="white" material="color: #fff; shader: flat;"></a-sphere>
+        </a-entity>
+        <a-entity position="-0.22 2.35 -0.68">
+          <a-entity light="type: spot; penumbra: 0.1; intensity: 0.2;" rotation="-90 0 0"></a-entity>
+          <a-sphere radius="0.05" color="white" material="color: #fff; shader: flat;"></a-sphere>
+        </a-entity>
         <a-entity environment="preset: egypt"></a-entity>
+        <a-camera position="-0.4 1.6 0.4"></a-camera>
+        <a-entity id="leftHand" hand-controls="hand: left; handModelStyle: lowPoly; color: #ffcccc"></a-entity>
+        <a-entity id="rightHand" hand-controls="hand: right; handModelStyle: lowPoly; color: #ffcccc"></a-entity>
       </a-scene>
     </div>
   );
