b417ccab8d1c9c20378138f5856228c264162153
Henry Holtgeerts <henryholtgeerts@gmail.com>
2022-02-26 13:46:36 -0600

    add scene support

--- src/App.js
+++ src/App.js
@@ -2,27 +2,49 @@ import './App.css';
 import { memo } from 'react';
 
 const App = memo(() => {
+
+
   return (
     <div className="App">
-      <a-scene>
-        <a-entity gltf-model="url(./closet.glb)" rotation="0 180 0" position="0.3 0 -0.4">
-          <a-entity position="0.22 2.35 0.68">
-            <a-entity light="type: spot; penumbra: 0.1; intensity: 0.2;" rotation="-90 0 0"></a-entity>
-            <a-sphere radius="0.05" color="white" material="color: #fff; shader: flat;"></a-sphere>
-          </a-entity>
-          <a-entity position="0.22 2.35 -0.21">
-            <a-entity light="type: spot; penumbra: 0.1; intensity: 0.2;" rotation="-90 0 0"></a-entity>
-            <a-sphere radius="0.05" color="white" material="color: #fff; shader: flat;"></a-sphere>
+      <a-scene cursor="rayOrigin: mouse">
+        <a-entity gltf-model="url(./closet-mod.glb)" rotation="0 180 0" position="0.3 0 -0.35">
+          <a-entity sound="src: url(./voiceover.mp3); autoplay: false;"></a-entity>
+            <a-entity id="lights">
+            <a-entity position="0.22 2.35 0.68">
+              <a-entity light="type: spot; penumbra: 0.1; intensity: 0.2;" rotation="-90 0 0"></a-entity>
+              <a-sphere radius="0.05" color="white" material="color: #fff; shader: flat;"></a-sphere>
+            </a-entity>
+            <a-entity position="0.22 2.35 -0.21">
+              <a-entity light="type: spot; penumbra: 0.1; intensity: 0.2;" rotation="-90 0 0"></a-entity>
+              <a-sphere radius="0.05" color="white" material="color: #fff; shader: flat;"></a-sphere>
+            </a-entity>
           </a-entity>
         </a-entity>
+        <a-box start-button width="0.3" height="0.3" depth="0.3" position="0 1.6 -1" color="yellow"></a-box>
         <a-box width="0.1" height="0.1" depth="0.1" color="red" position="0 0 0"></a-box>
-        <a-entity environment="preset: egypt"></a-entity>
-        <a-camera position="0 1.6 0"></a-camera>
+        <a-entity id="environment" visible="false" environment="preset: egypt"></a-entity>
+        <a-cylinder id="floor" radius="1.5" height="0.1" color="#6b512c" position="0 0 -0.5"></a-cylinder>
+        <a-cylinder id="ceiling" radius="1.5" height="0.1" color="white" position="0 2.85 -0.5"></a-cylinder>
+        <a-light id="ambientLight" type="ambient" intensity="0.6"></a-light>
+        <a-entity id="doors" animation__selldoors="property: visible; from: true; to: false; autoplay: false; startEvents: selldoors">
+          <a-box id="backdoor" width="1" height="2.6" depth="0.05" position="-0.1 1 0.63"></a-box>
+          <a-box id="sidedoor" width="1" height="2.6" depth="0.05" position="-0.72 1 -0.6" rotation="0 90 0"></a-box>
+        </a-entity>
+        <a-camera position="0 1.6 0">
+        </a-camera>
+        <a-entity 
+          id="friends"
+          visible="false"
+          animation__showfriends="property: visible; from: false; to: true; autoplay: false; startEvents: showfriends"
+          animation__hidefriends="property: visible; from: true; to: false; autoplay: false; startEvents: hidefriends">
+          <a-cone color="red" height="0.6" radius-bottom="0.2" position="0.4 1.6 -0.7" rotation="-11 22 -22"></a-cone>
+          <a-sphere color="blue" radius="0.3" position="0 1.6 -1.2"></a-sphere>
+          <a-box start-button width="0.3" height="0.3" depth="0.3" position="-0.4 1.6 -0.7" color="yellow" rotation="33 0 12"></a-box>
+        </a-entity>
         <a-entity id="leftHand" hand-controls="hand: left; handModelStyle: lowPoly; color: #ffcccc"></a-entity>
         <a-entity id="rightHand" hand-controls="hand: right; handModelStyle: lowPoly; color: #ffcccc"></a-entity>
       </a-scene>
     </div>
   );
 })
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,79 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'aframe';
 import 'aframe-environment-component';
+import 'super-hands';
+
+window.sceneStarted = false;
+
+window.AFRAME.registerComponent('start-button', {
+  init: function() {
+    const el = this.el;
+    el.addEventListener('click', function () {
+      el.setAttribute('visible', 'false');
+      window.startScene();
+    });
+  }
+})
+
+window.startScene = () => {
+    let currentTime = 0;
+    const tck = () => {
+      currentTime++;
+      console.log(currentTime)
+      switch ( currentTime ) {
+        case 20: {
+          document.querySelector("#friends").emit(`showfriends`, null, false);
+          break;
+        }
+        case 33: {
+          document.querySelector("#friends").emit(`hidefriends`, null, false);
+          break;
+        }
+        case 39: {
+          document.querySelector("#doors").emit(`selldoors`, null, false);
+          document.querySelector("#environment").setAttribute('visible', 'true');
+          document.querySelector("#environment").setAttribute('environment', {preset: 'default'});
+          document.querySelector("#ambientLight").setAttribute('intensity', '0')
+          break;
+        }
+        case 43: {
+          document.querySelector("#environment").setAttribute('environment', {preset: 'egypt'});
+          break;
+        }
+        case 44: {
+          document.querySelector("#environment").setAttribute('environment', {preset: 'arches'});
+          break;
+        }
+        case 46: {
+          document.querySelector("#environment").setAttribute('environment', {preset: 'moon'});
+          break;
+        }
+        case 51: {
+          document.querySelector("#ceiling").setAttribute('visible', "false")
+          break;
+        }
+        case 58: {
+          document.querySelector("#environment").setAttribute('environment', {preset: 'starry'});
+          document.querySelector("#lights").setAttribute('visible', 'false')
+          break;
+        }
+        case 72: {
+          document.querySelector("#environment").setAttribute('environment', {preset: 'default'});
+          document.querySelector("#lights").setAttribute('visible', 'true')
+          break;
+        }
+      }
+    }
+    if (window.sceneStarted === false) {
+      window.sceneStarted = true;
+      setInterval(tck, 1000);
+      var entity = document.querySelector('[sound]');
+      console.log('entity', entity)
+      entity.components.sound.playSound();
+      console.log('sound', entity.components.sound)
+    }
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
